4863748b19245fd7a09c8cae622d84c71d8ed6c6
Bruno Ricci <brunoriccidossantos@hotmail.com>
2024-01-27 20:31:02 -0300

    fix: remove config

--- models/index.js
+++ models/index.js
@@ -6,15 +6,9 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(process.env.DATABASE ?? 'brivao_aero', process.env.DB_USERNAME ?? 'root', process.env.DB_PASSWORD ?? 'root');
 
 fs
   .readdirSync(__dirname)
